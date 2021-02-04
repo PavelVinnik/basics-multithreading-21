a6347ec0e6d475757c3231de712968e9a4df1593
Pavel Vinnik <vinnik.pavel@gmail.com>
2021-02-04 14:53:34 +0200

    Done task with HandlerThread.

--- app/src/main/java/com/artemchep/basics_multithreading/MainActivity.java
+++ app/src/main/java/com/artemchep/basics_multithreading/MainActivity.java
@@ -1,7 +1,11 @@
 package com.artemchep.basics_multithreading;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
 import android.view.View;
+import android.widget.Switch;
 
 import androidx.annotation.UiThread;
 import androidx.appcompat.app.AlertDialog;
@@ -18,9 +22,16 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int CIPHER_DONE_MESSAGE = 1;
+
     private List<WithMillis<Message>> mList = new ArrayList<>();
 
     private MessageAdapter mAdapter = new MessageAdapter(mList);
+
+    private Handler mBackgroundHandler;
+    private HandlerThread mBackgroundThread;
+
+    private Handler mHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +43,23 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(mAdapter);
 
         showWelcomeDialog();
+
+        mHandler = new Handler(new Handler.Callback() {
+            @Override
+            public boolean handleMessage(android.os.Message msg) {
+                switch (msg.what) {
+                    case CIPHER_DONE_MESSAGE: {
+                        update((WithMillis<Message>) msg.obj);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+        mBackgroundThread = new HandlerThread("BackgroundThread");
+        mBackgroundThread.start();
+        mBackgroundHandler = new Handler(mBackgroundThread.getLooper());
     }
 
     private void showWelcomeDialog() {
@@ -39,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
                 .setMessage("What are you going to need for this task: Thread, Handler.\n" +
                         "\n" +
                         "1. The main thread should never be blocked.\n" +
-                        "2. Messages should be processed sequentially.\n" +
+                        "2. Messages ;should be processed sequentially.\n" +
                         "3. The elapsed time SHOULD include the time message spent in the queue.")
                 .show();
     }
@@ -56,6 +84,17 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO: Start processing the message (please use CipherUtil#encrypt(...)) here.
         //       After it has been processed, send it to the #update(...) method.
+        final long startTime = System.currentTimeMillis();
+
+        mBackgroundHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                final Message messageNew = message.value.copy(CipherUtil.encrypt(message.value.plainText));
+                long threadDuration = (System.currentTimeMillis() - startTime);
+                final WithMillis<Message> messageNewWithMillis = new WithMillis<>(messageNew, threadDuration);
+                mHandler.sendMessage(mHandler.obtainMessage(CIPHER_DONE_MESSAGE, messageNewWithMillis));
+            }
+        });
 
         // How it should look for the end user? Uncomment if you want to see. Please note that
         // you should not use poor decor view to send messages to UI thread.
@@ -82,4 +121,9 @@ public class MainActivity extends AppCompatActivity {
         throw new IllegalStateException();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mBackgroundThread.quit();
+    }
 }
